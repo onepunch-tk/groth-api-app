eb6120c483a86934e102414b6de9f700dd9f50c7
TaekyungHa <86tkstar@gmail.com>
2024-02-15 19:14:30 +0900

    add auth service logic

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,26 @@
+import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Role, Social } from '../interfaces/social-user.interface';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ unique: true })
+  username: string;
+  @Prop()
+  password: string;
+  @Prop()
+  nickname: string;
+  @Prop({ enum: Social, type: String })
+  social: Social;
+  @Prop({
+    enum: Role,
+    type: String,
+    default: Role.USER,
+    required: false,
+  })
+  role: Role;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
